e5d036b4fbdab83409dd5d4914bfbae23aa431ad
jakaria98 <jhossain439@gmail.com>
2024-03-18 16:58:03 -0230

    header and footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2024 Skier Mesh. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
